88a101071cf11fe7e093f25c8f6678133e90ed81
ewaaaaannnn <ewanferg01@gmail.com>
2024-12-06 11:06:49 -0700

    filled out env and formatted

---
+++ src/components/PostList.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <p>ope</p>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <p></p>
+</template>
+
+<style scoped lang="scss"></style>
